46948768e87077ca6896e80a9f921694652d7a17
OctopusUnity <98890960+OctopusUnity@users.noreply.github.com>
2022-04-09 17:33:29 +0700

    Olga
    Скрипт для разговора Ольги

---
+++ Museum/Assets/Olga_Talking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class Olga_Talking : MonoBehaviour
+{
+    public GameObject Olga;
+    private VideoPlayer video;
+    // Start is called before the first frame update
+    void Start()
+    {
+        video = Olga.GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Olga"))
+        {
+            video.Play();
+        }
+    }
+}
