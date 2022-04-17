2757417f96d263818ec6e21b4d859b9b92e12d0d
zaisusa <vinnikovvanya123@gmail.com>
2022-04-17 16:28:55 +0700

    add interaction with ui

--- Museum/Assets/Pointer.cs
+++ Museum/Assets/Pointer.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Pointer : MonoBehaviour
 {
+    public float lenght = 5f;
+    public GameObject Dot;
+    public VRInputModule m_inputmModule;
+
+    private LineRenderer line = null;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        line = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        PointerEventData data = m_inputmModule.GetData();
+
+        float taregLenght = data.pointerCurrentRaycast.distance == 0 ? lenght : data.pointerCurrentRaycast.distance;
+        RaycastHit hit = CreateRaycast(lenght);
+        Vector3 endpos = transform.position + (transform.forward * taregLenght);
+        if (hit.collider != null)
+            endpos = hit.point;
+
+        Dot.transform.position = endpos;
+
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, endpos);
+    }
+
+    private RaycastHit CreateRaycast(float lenght)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, lenght);
+
+        return hit;
+
     }
 }
---
+++ Museum/Assets/VRInputModule.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class VRInputModule : BaseInputModule
+{
+    public Camera cam;
+    public SteamVR_Input_Sources targSource;
+    public SteamVR_Action_Boolean clcikAct;
+
+    private GameObject curObj;
+    private PointerEventData data;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        data = new PointerEventData(eventSystem);
+    }
+    public override void Process()
+    {
+        data.Reset();
+        data.position = new Vector2(cam.pixelWidth / 2, cam.pixelHeight / 2);
+
+        eventSystem.RaycastAll(data, m_RaycastResultCache);
+        data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        curObj = data.pointerCurrentRaycast.gameObject;
+
+        m_RaycastResultCache.Clear();
+
+        HandlePointerExitAndEnter(data, curObj);
+
+        if (clcikAct.GetStateDown(targSource))
+        {
+            ProcessPress(data);
+        }
+
+        if (clcikAct.GetStateUp(targSource))
+        {
+            ProcessRelease(data);
+        }
+
+    }
+
+    public PointerEventData GetData()
+    {
+        return data;
+    }
+    private void ProcessPress(PointerEventData data)
+    {
+        data.pointerPressRaycast = data.pointerCurrentRaycast;
+        GameObject newPointerpress = ExecuteEvents.ExecuteHierarchy(curObj, data, ExecuteEvents.pointerDownHandler);
+        if (newPointerpress == null)
+        {
+            newPointerpress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(curObj);
+
+        }
+
+        data.pressPosition = data.position;
+        data.pointerPress = newPointerpress;
+        data.rawPointerPress = curObj;
+    }
+
+    private void ProcessRelease(PointerEventData data)
+    {
+        ExecuteEvents.Execute(data.pointerEnter, data, ExecuteEvents.pointerUpHandler);
+        GameObject poinnterUpHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(curObj);
+
+        if(data.pointerPress == poinnterUpHandler)
+        {
+            ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerClickHandler);
+        }
+
+        eventSystem.SetSelectedGameObject(null);
+
+        data.pressPosition = Vector2.zero;
+        data.pointerPress = null;
+        data.rawPointerPress = null;
+
+    }
+}
