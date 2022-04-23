ada82cc490d3b5908ecfb69f217fe545da1eff24
zaisusa <vinnikovvanya123@gmail.com>
2022-04-23 14:05:17 +0700

    test scene + walls

---
+++ Museum/Assets/scripts/TP_Player_for_button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_Player_for_button : MonoBehaviour
+{
+    public GameObject go;
+    Vector3 vec = new Vector3(0.5f, 1.2f, 2.3f);
+    float i = 1.0f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void spawn()
+    {
+        Instantiate(go, new Vector3(i * 0.5f, 1.2f, 2.3f), Quaternion.identity);
+    }
+}
